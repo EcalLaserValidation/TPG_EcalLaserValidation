5a6e8b1e67b6f4b27c37b8d127d95dff2ce4f31b
taroni <Silvia.Taroni@cern.ch>
2018-03-17 17:39:34 +0100

    new.sh updated to push in HLT_EcalLaserValidation

--- new.sh
+++ new.sh
@@ -8,11 +8,22 @@ echo ToRun/$file
     week=`grep "week" ToRun/$file | awk '{print $2}'`
     sqlite1=`grep "run1" ToRun/$file | awk '{print $2}'`
     sqlite2=`grep "run2" ToRun/$file | awk '{print $2}'`
-mv ToRun/$file RunFiles/.
-echo "./tpganalysis_jenkins_2018.sh $sqlite1 $sqlite2 $week $year"
-./tpganalysis_jenkins_2018.sh $sqlite1 $sqlite2 $week $year 
-git commit -a -m "clean ToRun files"
+cd ..
+rm -rf HLT_EcalLaserValidation
+git init
+git clone https://github.com/EcalLaserValidation/HLT_EcalLaserValidation.git
+#git clone ssh://git@github.com/EcalLaserValidation/HLT_EcalLaserValidation.git
+cd HLT_EcalLaserValidation
+cp ../TPG_EcalLaserValidation/ToRun/$file ToRun/NewToRun.txt
+git commit -a -m "update ToRun files: trigger a new validation for IoV=$sqlite2"
+git remote set-url origin ssh://git@github.com/EcalLaserValidation/HLT_EcalLaserValidation.git
 git push
+#cd ../TPG_EcalLaserValidation 
+#mv ToRun/$file RunFiles/.
+echo "./tpganalysis_jenkins_2018.sh $sqlite1 $sqlite2 $week $year"
+#./tpganalysis_jenkins_2018.sh $sqlite1 $sqlite2 $week $year 
+#git commit -a -m "clean ToRun files"
+#git push
 else
 echo "No new files"
 fi
