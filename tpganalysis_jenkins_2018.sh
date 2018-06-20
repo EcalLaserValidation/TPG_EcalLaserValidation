f05d89a55ba27e72c90cc9ce6be16c724a3bc788
taroni <zghiche@cern.ch>
2018-06-20 14:38:24 +0200

    release 10_1_5 dataset 316766

--- tpganalysis_jenkins_2018.sh
+++ tpganalysis_jenkins_2018.sh
@@ -6,10 +6,13 @@ echo "reference and test sqlite files"
 echo "usage: ./tpganalysis_jenkins_2018.sh $sqlite1 $sqlite2 $week $year $(getconf _NPROCESSORS_ONLN)"
 
 ###############################
-dataset=Run2017D/SinglePhoton/RAW/v1
+#dataset=Run2017D/SinglePhoton/RAW/v1
+dataset=Run2018A/EGamma/RAW/v1
 #GT=94X_dataRun2_v2
-GT=100X_dataRun2_v1
-reference=302635
+#GT=100X_dataRun2_v1
+GT=101X_dataRun2_Prompt_v9
+#reference=302635
+reference=316766
 sqlite1=$1
 sqlite2=$2
 week=$3
@@ -23,8 +26,10 @@ datasetpath=`echo ${dataset} | tr '/' '_'`
 
 #export CMSREL=CMSSW_9_4_0_pre3
 #export RELNAME=TPLasVal_940pre3
-export CMSREL=CMSSW_10_0_0
-export RELNAME=TPLasVal_1000
+#export CMSREL=CMSSW_10_0_0
+#export RELNAME=TPLasVal_1000
+export CMSREL=CMSSW_10_1_5
+export RELNAME=TPLasVal_1015
 export SCRAM_ARCH=slc6_amd64_gcc630
 
 if ${INSTALL}; then
@@ -37,9 +42,12 @@ git cms-init
 ##oldgit fetch cms-l1t-offline
 #oldgit fetch https://github.com/cms-l1t-offline/cmssw.git
 #oldgit cms-merge-topic -u cms-l1t-offline:l1t-integration-v97.1
+# git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
+# git fetch cms-l1t-offline l1t-integration-CMSSW_10_0_0
+# git cms-merge-topic -u cms-l1t-offline:l1t-integration-v97.17-v2
 git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
-git fetch cms-l1t-offline l1t-integration-CMSSW_10_0_0
-git cms-merge-topic -u cms-l1t-offline:l1t-integration-v97.17-v2
+git fetch cms-l1t-offline l1t-integration-CMSSW_10_1_5
+git cms-merge-topic -u cms-l1t-offline:l1t-integration-v98.4
 
 git cms-addpkg L1Trigger/L1TCommon
 git cms-addpkg L1Trigger/L1TMuon
@@ -47,6 +55,8 @@ git cms-addpkg L1Trigger/L1TMuon
 git clone https://github.com/cms-l1t-offline/L1Trigger-L1TMuon.git L1Trigger/L1TMuon/data
 # git cms-addpkg L1Trigger/L1TCalorimeter #not sure if needed
 # git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data #gives an error
+git cms-addpkg L1Trigger/L1TCalorimeter
+git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
 
 scram b -j $(getconf _NPROCESSORS_ONLN)
 #cp -r /eos/cms/store/caf/user/ecaltrg/EcalTPGAnalysis .
@@ -62,25 +72,25 @@ if ${RUN}; then
 wget http://cern.ch/ecaltrg/EcalLin/EcalTPG_${sqlite1}_moved_to_1.db
 wget http://cern.ch/ecaltrg/EcalLin/EcalTPG_${sqlite2}_moved_to_1.db
 
-#./runTPGbatchLC_jenkins_2018.sh jenkins $reference $dataset $GT $nevents $sqlite1 $(getconf _NPROCESSORS_ONLN) &
-#wait
+./runTPGbatchLC_jenkins_2018.sh jenkins $reference $dataset $GT $nevents $sqlite1 $(getconf _NPROCESSORS_ONLN) &
+wait
 ./runTPGbatchLC_jenkins_2018.sh jenkins $reference $dataset $GT $nevents $sqlite2 $(getconf _NPROCESSORS_ONLN) &
 wait
 fi
-#cp addhist_jenkins_2018.sh log_and_results/${reference}-${datasetpath}-LC-IOV_${sqlite1}-batch/.
-#pushd log_and_results/${reference}-${datasetpath}-LC-IOV_${sqlite1}-batch/
-#./addhist_jenkins_2018.sh ${sqlite1} &
-#popd
+cp addhist_jenkins_2018.sh log_and_results/${reference}-${datasetpath}-LC-IOV_${sqlite1}-batch/.
+pushd log_and_results/${reference}-${datasetpath}-LC-IOV_${sqlite1}-batch/
+./addhist_jenkins_2018.sh ${sqlite1} &
+popd
 cp addhist_jenkins_2018.sh log_and_results/${reference}-${datasetpath}-LC-IOV_${sqlite2}-batch/.
 pushd log_and_results/${reference}-${datasetpath}-LC-IOV_${sqlite2}-batch/
 ./addhist_jenkins_2018.sh ${sqlite2} &
 popd
 wait
 
-#mv log_and_results/${reference}-${datasetpath}-LC-IOV_${sqlite1}-batch/newhistoTPG_${sqlite1}_eg12.root ../../TPGPlotting/plots/.
+mv log_and_results/${reference}-${datasetpath}-LC-IOV_${sqlite1}-batch/newhistoTPG_${sqlite1}_eg12.root ../../TPGPlotting/plots/.
 
-wget http://cern.ch/ecaltrg/ReferenceNTuples/TPG/newhistoTPG_${sqlite1}_eg12.root  
-mv newhistoTPG_${sqlite1}_eg12.root ../../TPGPlotting/plots/.
+#wget http://cern.ch/ecaltrg/ReferenceNTuples/TPG/newhistoTPG_${sqlite1}_eg12.root  
+#mv newhistoTPG_${sqlite1}_eg12.root ../../TPGPlotting/plots/.
 
 mv log_and_results/${reference}-${datasetpath}-LC-IOV_${sqlite2}-batch/newhistoTPG_${sqlite2}_eg12.root ../../TPGPlotting/plots/.
 
