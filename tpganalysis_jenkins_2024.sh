b3ba1dfc3ab863008ebb8409c4a1984decea1a7e
Zghiche <amina.zghiche@cern.ch>
2024-05-14 11:10:58 +0200

    again

--- tpganalysis_jenkins_2024.sh
+++ tpganalysis_jenkins_2024.sh
@@ -40,7 +40,7 @@ git cms-addpkg L1Trigger/L1TNtuples/python
 git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data 
 
 sed 's/+l1UpgradeTfMuonTree/#+l1UpgradeTfMuonTree/g' L1Trigger/L1TNtuples/python/L1NtupleRAW_cff.py
-sed 's/+l1TauRecoTree/#+l1TauRecoTree/g' L1Trigger/L1TNtuples/python/L1Ntuple/L1NtupleAOD_cff.py
+sed 's/+l1TauRecoTree/#+l1TauRecoTree/g' L1Trigger/L1TNtuples/python/L1NtupleAOD_cff.py
 git cms-checkdeps -A -a
 
 scram b -j $(getconf _NPROCESSORS_ONLN)
